534d0eddb34c221c574241c903cbee44e4a6b4b8
Enzo <enzo@gautier.it>
2023-02-24 23:31:02 +0900

    Add logs Convert dump DB code into functions Edit messsage output

--- backup.sh
+++ backup.sh
@@ -13,54 +13,79 @@ if [ "$1" != "--backup" ] && [ "$1" != "--install" ]; then
   exit 1
 fi
 
+# VARIABLES 
 #############
-# VARIABLES #
-#############
-
-# Define the root directory
-ROOT_DIR="$HOME"
-
-# Define the path to the script backup directory
-RESTIC_SCRIPT="$ROOT_DIR/scripts/backup"
-
-# Define the directory to store database backups
-DB_BACKUP_DIR="$ROOT_DIR/backup-db"
 
 # Add path of binary during script execution
-export PATH=$PATH:$RESTIC_SCRIPT
+export PATH=$PATH:$DIR_SCRIPT_BACKUP
+# Define the root directory
+DIR_ROOT="$HOME"
+# Define the directory of your wordpress installations - default is $HOME
+DIR_WP="$DIR_ROOT"
+# Define the directory to store database dump
+DIR_DB_BACKUP="$DIR_ROOT/backup-db"
+# Define the path of the scripts directory
+DIR_SCRIPTS="$DIR_ROOT/scripts"
+# Define the path to the script backup directory
+DIR_SCRIPT_BACKUP="$DIR_SCRIPTS/backup"
 
+# Define the directory to store logs
+DIR_SCRIPT_LOGS="$DIR_SCRIPT_BACKUP/logs"
 # Define the path to the Rclone binary
-RCLONE_BIN="$RESTIC_SCRIPT/rclone"
-
+RCLONE_BIN="$DIR_SCRIPT_BACKUP/rclone"
 # Define the path to the Restic binary
-RESTIC_BIN="$RESTIC_SCRIPT/restic"
-
+RESTIC_BIN="$DIR_SCRIPT_BACKUP/restic"
 #Define the path of restic configuration
-RESTIC_CONF="$RESTIC_SCRIPT/backup-restic-conf.txt"
-
+RESTIC_CONF="$DIR_SCRIPT_BACKUP/backup-restic-conf.txt"
 # Set the path to the Restic password file
-restic_password_file="$RESTIC_SCRIPT/backup-restic-pwd.txt"
-
+RESTIC_PWD_FILE="$DIR_SCRIPT_BACKUP/backup-restic-pwd.txt"
 # Define the file containing other databases to backup and usernames
-OTHER_DBS_FILE="$RESTIC_SCRIPT/backup-db-others.txt"
-
+OTHER_DBS_FILE="$DIR_SCRIPT_BACKUP/backup-db-others.txt"
 # Define the file containing the directories to exclude
-EXCLUDED_DIRS_FILE="$RESTIC_SCRIPT/backup-excluded-dirs.txt"
+EXCLUDED_DIRS_FILE="$DIR_SCRIPT_BACKUP/backup-excluded-dirs.txt"
+#Define log file name
+LOG_FILE=$(date +"%Y-%m-%d-%H-%M")"_backup.log"
+#Define how many days we keep the log files
+LOG_DAYS_TO_KEEP=90
 
-################
-#  FUNCTIONS   #
-################
+# FUNCTIONS
+#############
 
-# Define function to create .htaccess file if it doesn't exist
+# Define function to create .htaccess file if it doesn't exist, overwrite it if the content is not correct.
 function create_htaccess_file() {
   # Define the path to the .htaccess file
   HTACCESS_FILE="$1/.htaccess"
+  DESIRED_CONTENT="deny from all"
 
-  # Create the .htaccess file if it does not exist
-    if [ ! -f "$HTACCESS_FILE" ]; then
-    echo "deny from all" > "$HTACCESS_FILE"
-    echo ".htaccess file created at $HTACCESS_FILE"
+  if [ -f "$HTACCESS_FILE" ]; then
+    # If the file exists, check if its content matches the desired content
+    if [ "$(cat "$HTACCESS_FILE")" == "$DESIRED_CONTENT" ]; then
+      # If the content matches, display a message and return
+      echo "[✓] Content up-to-date for: $HTACCESS_FILE"
+      return
+    else
+      # If the content is different, display a message and overwrite the file
+      echo "$DESIRED_CONTENT" > "$HTACCESS_FILE"
+      echo "[!] Content updated to correct value for $HTACCESS_FILE"
+      return
+    fi
+  else
+    # If the file does not exist, create it with the desired content and display a message
+    echo "[X] File missing: $HTACCESS_FILE."
+    echo "$DESIRED_CONTENT" > "$HTACCESS_FILE"
+    if [ -f "$HTACCESS_FILE" ]; then
+      echo "[!] File created: $HTACCESS_FILE."
+    else
+      echo "[X] Error: Failed to create .htaccess file: $HTACCESS_FILE"
+      exit 1
+    fi
+    return
   fi
+}
+
+
+function delete_old_logs() {
+  find "$DIR_SCRIPT_LOGS" -type f -name "*.log" -mtime +$LOG_DAYS_TO_KEEP -delete
 }
 
 function install_rclone {
@@ -69,10 +94,10 @@ function install_rclone {
 
   # Download the latest Restic release
   echo "Downloading latest release of Rclone..."
-  curl -L -o $RESTIC_SCRIPT/rclone.zip $LATEST_RCLONE
-  unzip -j $RESTIC_SCRIPT/rclone.zip "*rclone" -d $RESTIC_SCRIPT
-  chmod +x $RESTIC_SCRIPT/rclone
-  rm $RESTIC_SCRIPT/rclone.zip
+  curl -L -o $DIR_SCRIPT_BACKUP/rclone.zip $LATEST_RCLONE
+  unzip -j $DIR_SCRIPT_BACKUP/rclone.zip "*rclone" -d $DIR_SCRIPT_BACKUP
+  chmod +x $DIR_SCRIPT_BACKUP/rclone
+  rm $DIR_SCRIPT_BACKUP/rclone.zip
   echo "Latest release of Rclone downloaded"
 }
 
@@ -82,9 +107,9 @@ function install_restic {
 
   # Download the latest Restic release
   echo "Downloading latest release of Restic..."
-  curl -L -o $RESTIC_SCRIPT/restic.bz2 $LATEST_RESTIC
-  bunzip2 $RESTIC_SCRIPT/restic.bz2
-  chmod +x $RESTIC_SCRIPT/restic
+  curl -L -o $DIR_SCRIPT_BACKUP/restic.bz2 $LATEST_RESTIC
+  bunzip2 $DIR_SCRIPT_BACKUP/restic.bz2
+  chmod +x $DIR_SCRIPT_BACKUP/restic
   echo "Latest release of Restic downloaded"
 }
 
@@ -104,8 +129,8 @@ function create_mandatory_dir() {
 
 # Function to check if script is located in the backup directory
 function is_script_in_backup_dir() {
-  # Check if the script is located in the backup directory "$HOME/scripts/backup"
-  if [ "$(dirname "$(realpath "$0")")" = "$RESTIC_SCRIPT" ]; then
+  # Check if the script is located in the backup directory "$DIR_SCRIPT_BACKUP"
+  if [ "$(dirname "$(realpath "$0")")" = "$DIR_SCRIPT_BACKUP" ]; then
     is_script_in_backup_dir=true
   else
     is_script_in_backup_dir=false
@@ -116,18 +141,18 @@ function copy_script_to_backup_dir() {
   # Get the current location of the script
   SCRIPT_LOCATION=$(realpath "$0")
   # Copy the script to the backup directory
-    cp "$SCRIPT_LOCATION" "$RESTIC_SCRIPT/"
-  chmod +x "$RESTIC_SCRIPT/backup.sh"
+    cp "$SCRIPT_LOCATION" "$DIR_SCRIPT_BACKUP/"
+  chmod +x "$DIR_SCRIPT_BACKUP/backup.sh"
 }
 
 function create_db_others_file() {
   # Check if the db-others file exists, if not create it with sample content
   if [ ! -f "$OTHER_DBS_FILE" ]; then
-    echo "# To backup other databases not related to WordPress, add lines to this file in the following format:
-    # dbname;username;password
-    # Example:
-    # mydb1;myuser1;mypassword1
-    # mydb2;myuser2;mypassword2" > "$OTHER_DBS_FILE"
+    echo "# To backup other databases not related to WordPress, add lines to this file in the following format:" \
+    "# dbname;username;password" \
+    "# Example:" \
+    "# mydb1;myuser1;mypassword1" \
+    "# mydb2;myuser2;mypassword2" > "$OTHER_DBS_FILE"
   fi
 }
 
@@ -153,94 +178,117 @@ function create_restic_files (){
   fi
 
   # Create the Restic password file if it doesn't exist and add sample content
-  if [ ! -f "$restic_password_file" ]; then
-    echo "INPUT_YOUR_RESTIC_REPO_PASSWORD_HERE" > "$restic_password_file"
-    echo "Restic password file created at $restic_password_file, edit it before to launch the backup script again"
+  if [ ! -f "$RESTIC_PWD_FILE" ]; then
+    echo "INPUT_YOUR_RESTIC_REPO_PASSWORD_HERE" > "$RESTIC_PWD_FILE"
+    echo "Restic password file created at $RESTIC_PWD_FILE, edit it before to launch the backup script again"
     exit 1
   fi
 }
 
 function check_required_files() {
-  local files=("$DB_BACKUP_DIR" "$RESTIC_BIN" "$RCLONE_BIN" "$RESTIC_CONF" "$restic_password_file" "$OTHER_DBS_FILE" "$EXCLUDED_DIRS_FILE")
-  for file in "${files[@]}"; do
-    if [ ! -e "$file" ]; then
-      echo "Error: $file does not exist. All file declared within the function check_required_files have to be present"
-      exit 1
+  local paths=("$@")
+  local error=0
+  for path in "${paths[@]}"; do
+    if [ -e "$path" ]; then
+      if [ -d "$path" ]; then
+        echo "[✓] Directory found: $path "
+      else
+        echo "[✓] File found: $path "
+      fi
+    else
+      if [ -d "$path" ]; then
+        echo "[X] Directory not found: $path"
+      else
+        echo "[X] File not found: $path"
+      fi
+      error=1
+    fi
+  done
+  if [ $error -eq 1 ]; then
+    exit 1
+  fi
+}
+
+function dump_wordpress_databases() {
+  local ROOT_DIR=""
+  local BACKUP_DIR=""
+  local count=0
+
+  # Parse arguments
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      --root-dir=*)
+        ROOT_DIR="${1#*=}"
+        ;;
+      --backup-dir=*)
+        BACKUP_DIR="${1#*=}"
+        ;;
+      *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+  done
+
+  # Check that both arguments are present
+  if [ -z "$ROOT_DIR" ] || [ -z "$BACKUP_DIR" ]; then
+    echo "Usage: dump_wordpress_databases --root-dir=ROOT_DIR --backup-dir=BACKUP_DIR"
+    exit 1
+  fi
+
+  # Count the number of WordPress installations in $ROOT_DIR
+  for INSTALLATION_DIR in "$ROOT_DIR"/*/; do
+    if [ -f "$INSTALLATION_DIR/wp-config.php" ]; then
+      count=$((count+1))
+    fi
+  done
+
+  # Output the number of installations found or a message if none are found
+  if [ $count -eq 0 ]; then
+    echo "No WordPress installations found in $ROOT_DIR"
+  else
+    echo "$count WordPress DB found"
+  fi
+
+  # Dump the databases for each WordPress installation
+  for INSTALLATION_DIR in "$ROOT_DIR"/*/; do
+    if [ -f "$INSTALLATION_DIR/wp-config.php" ]; then
+      echo "Starting to dump the DB for WordPress installation detected: $INSTALLATION_DIR"
+      # Extract the database connection details from wp-config.php
+      DATABASE=$(grep -oP "define\(\s*'DB_NAME'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/wp-config.php")
+      DB_USER=$(grep -oP "define\(\s*'DB_USER'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/wp-config.php")
+      DB_PASSWORD=$(grep -oP "define\(\s*'DB_PASSWORD'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/wp-config.php")
+      # If a database name is found, create a backup file
+      if [ -n "$DATABASE" ]; then
+        DATE=$(date +"%Y-%m-%d")
+        TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+        DUMP_FILE="${DATABASE}_${DATE}_${TIMESTAMP}.sql"
+        if mysqldump --user="$DB_USER" --password="$DB_PASSWORD" --databases "$DATABASE" > "$BACKUP_DIR/$DUMP_FILE"; then
+          gzip "$BACKUP_DIR/$DUMP_FILE"
+          echo "[✓] Dump succeed for $INSTALLATION_DIR"
+        else
+          echo "[X] Dump failed for: $INSTALLATION_DIR"
+        fi
+      fi
     fi
   done
 }
 
-############################
-#Installation of the script#
-############################
+dump_mysql_dbs() {
+  local FILE="$1"
+  local NUM_DBS=0
 
-if [ "$1" = "--install" ]; then
-  echo "Starting the install of restic backup..."
-  create_mandatory_dir "$ROOT_DIR/scripts" "check"
-  create_mandatory_dir "$RESTIC_SCRIPT" "check"
-  create_htaccess_file "$RESTIC_SCRIPT"
-  create_mandatory_dir "$DB_BACKUP_DIR"
-  create_htaccess_file "$DB_BACKUP_DIR"
-  install_restic
-  install_rclone
-  create_db_others_file
-  create_file_exclude_directory
-  create_restic_files
-  if [ "$is_script_in_backup_dir" = false ]; then
-      echo "We copy the script in the dir:$RESTIC_SCRIPT"
-      copy_script_to_backup_dir
-      echo "You can now delete the script from $SCRIPT_LOCATION"
-  fi
-
-fi
-
-############################
-#        Backup            #
-############################
-
-if [ "$1" = "--backup" ]; then
-  my_date=$(date +"%Y-%m-%d %H:%M")
-  echo "Backup script is now starting -  $my_date"
-  # Call the check_script_location function
-  is_script_in_backup_dir
-  if [ "$is_script_in_backup_dir" = true ]; then
-      echo "The script is located in the backup directory as it should."
-  else
-      echo "The script is not located in the backup directory. Executed the script from the backup directory location: $RESTIC_SCRIPT"
-      exit 1
-  fi
-  #Protect the script directory - prevent access from the web
-  echo "Check if a .htaccess file protect the direcotry $RESTIC_SCRIPT"
-  create_htaccess_file "$RESTIC_SCRIPT"
-  echo "Check if a .htaccess file protect the direcotry $DB_BACKUP_DIR"
-  create_htaccess_file "$DB_BACKUP_DIR"
-  echo "Check if all files needed for the script to execute properly are present"
-  check_required_files
-
-  # Loop over directories in the root directory to find wordpress installation and dump their DB
-  for INSTALLATION_DIR in "$ROOT_DIR"/*/; do
-    if [ -f "$INSTALLATION_DIR/wp-config.php" ]; then
-        echo "Starting to dump the DB for WordPress installation detected: $INSTALLATION_DIR"
-        DATABASE=$(grep "define('DB_NAME'" "$INSTALLATION_DIR/wp-config.php" | cut -d "'" -f 4)
-        DB_USER=$(grep "define('DB_USER'" "$INSTALLATION_DIR/wp-config.php" | cut -d "'" -f 4)
-        DB_PASSWORD=$(grep "define('DB_PASSWORD'" "$INSTALLATION_DIR/wp-config.php" | cut -d "'" -f 4)
-        if [ -n "$DATABASE" ]; then
-            DATE=$(date +"%Y-%m-%d")
-            TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-            DUMP_FILE="${DATABASE}_${DATE}_${TIMESTAMP}.sql"
-            mysqldump --user=$DB_USER --password=$DB_PASSWORD --databases $DATABASE > "$DB_BACKUP_DIR/$DUMP_FILE"
-            gzip "$DB_BACKUP_DIR/$DUMP_FILE"
-        fi
-    fi
-  done
-  
+  echo "Check if specific DB has been listed for backup:"
   # Backup databases not related to WordPress installation
-  if [ -f "$OTHER_DBS_FILE" ] && [ -s "$OTHER_DBS_FILE" ]; then
-  echo "Start to dump the databases present in the file $OTHER_DBS_FILE"
-  while read -r LINE; do
+  if [ -f "$FILE" ] && [ -s "$FILE" ]; then
+    NUM_DBS=$(grep -v "^#" "$FILE" | wc -l)
+    echo "$NUM_DBS DB to backup based on $FILE"
+
+    while read -r LINE; do
       if [[ "$LINE" == \#* ]]; then
-      # Skip commented lines
-      continue
+        # Skip commented lines
+        continue
       fi
       DB_NAME=$(echo "$LINE" | cut -d ";" -f 1)
       DB_USER=$(echo "$LINE" | cut -d ";" -f 2)
@@ -248,46 +296,103 @@ if [ "$1" = "--backup" ]; then
       DATE=$(date +"%Y-%m-%d")
       TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
       DUMP_FILE="${DB_NAME}_${DATE}_${TIMESTAMP}.sql"
-      mysqldump --user=$DB_USER --password=$DB_PASSWORD --databases $DB_NAME > "$DB_BACKUP_DIR/$DUMP_FILE"
-      gzip "$DB_BACKUP_DIR/$DUMP_FILE"
-  done < "$OTHER_DBS_FILE"
+      if mysqldump --user=$DB_USER --password=$DB_PASSWORD --databases $DB_NAME > "$DIR_DB_BACKUP/$DUMP_FILE"; then
+        echo "[✓] Dump succeed for: $DB_NAME"
+        gzip "$DIR_DB_BACKUP/$DUMP_FILE"
+      else
+        echo "[X] Dump failed for: $DB_NAME"
+      fi
+    done < "$FILE"
   else
-  echo "The is no other databases dump required based on the file $OTHER_DBS_FILE"
+    echo "[✓] No other DB dump required based on: $FILE"
+  fi
+}
+
+
+# SCRIPT'S INSTALLATION
+#########################
+# The sequence below is executed if the --install argument is being used.
+if [ "$1" = "--install" ]; then
+  echo "Starting the install of restic backup..."
+  create_mandatory_dir "$DIR_SCRIPTS" "check"
+  create_mandatory_dir "$DIR_SCRIPT_BACKUP" "check"
+  create_mandatory_dir "$DIR_SCRIPT_LOGS" "check"
+  create_mandatory_dir "$DIR_DB_BACKUP"
+  create_htaccess_file "$DIR_SCRIPT_BACKUP"  
+  create_htaccess_file "$DIR_DB_BACKUP"
+  install_restic
+  install_rclone
+  create_db_others_file
+  create_file_exclude_directory
+  create_restic_files
+  if [ "$is_script_in_backup_dir" = false ]; then
+      echo "We copy the script in the dir:$DIR_SCRIPT_BACKUP"
+      copy_script_to_backup_dir
+      echo "Now deleting the script from $SCRIPT_LOCATION"
+      rm "$(realpath "$0")"
+  fi
+fi
+
+# BACKUP  
+##########
+# The sequence below is executed if the --backup argument is being used.
+
+if [ "$1" = "--backup" ]; then
+
+  # Redirect all output to the log file and the terminal
+  exec &> >(tee -a "$DIR_SCRIPT_LOGS/$LOG_FILE")
+  
+  my_date=$(date +"%Y-%m-%d %H:%M")
+  echo "Backup script is now starting -  $my_date"
+  # Call the check_script_location function
+  echo "Check script location:"
+  is_script_in_backup_dir
+  if [ "$is_script_in_backup_dir" = true ]; then
+      echo "[✓] Script located in: $DIR_SCRIPT_BACKUP"
+  else
+      echo "[X] Script have to be executed from: $DIR_SCRIPT_BACKUP"
+      exit 1
   fi
 
-  # Read the excluded directories from the file
-  if [ -f "$EXCLUDED_DIRS_FILE" ]; then
-    echo "Reading excluded directories from file: $EXCLUDED_DIRS_FILE"
-  else
-    echo "Error: excluded directories file not found: $EXCLUDED_DIRS_FILE"
-    exit 1
-  fi
+  echo "Check if all directories needed are present:"
+  check_required_files "$DIR_SCRIPT_LOGS" "$DIR_DB_BACKUP"
+  echo "Check if all files needed are present:"
+  check_required_files  "$RESTIC_BIN" "$RCLONE_BIN" "$RESTIC_CONF" "$RESTIC_PWD_FILE" "$OTHER_DBS_FILE" "$EXCLUDED_DIRS_FILE"
+  #Protect the script directory - prevent access from the web
+  echo "Check .htaccess files content: "
+  create_htaccess_file "$DIR_SCRIPT_BACKUP"
+  create_htaccess_file "$DIR_DB_BACKUP"
+  #Dump WP DB 
+  dump_wordpress_databases --root-dir=$DIR_WP --backup-dir=$DIR_DB_BACKUP
+  #Dump other MySQL DB if any listed
+  dump_mysql_dbs $OTHER_DBS_FILE
+  # Import the list of excluded directories to not backup.
+  echo "Check folders and files excluded from backup."
   while read -r line; do
     if [[ "$line" != \#* ]]; then
       exclude_flags+=" --exclude $line"
     fi
   done < "$EXCLUDED_DIRS_FILE"
-
-  # Load Restic configuration
+  # Load RESTIC CONF
+  echo "Load Restic repository to use for the backup."
   if [ -f "$RESTIC_CONF" ]; then
-  . "$RESTIC_CONF"
+    . "$RESTIC_CONF"
   else
-  echo "Restic configuration file not found: $RESTIC_CONF"
-  exit 1
+    echo "[X] Restic configuration file not found: $RESTIC_CONF"
+    exit 1
   fi
-
-  # Create a backup with Restic for each directory
-  echo "We start to backup the files to the external repository with restic.."
-  restic backup $ROOT_DIR --repo $restic_repo -p $restic_password $exclude_flags
-
+  # Restic will backup all directories located in $DIR_ROOT except the one listed for exclusion.
+  echo "Start to backup your data to your restic repo."
+  restic backup $DIR_ROOT --repo $restic_repo -p $RESTIC_PWD_FILE $exclude_flags
   # On the 15th of the month we clean snapshot older than 3 months and we prune the repo
   if [ "$(date +%d)" -eq 15 ]; then
   # Remove snapshot older than 3 months
-  restic forget --keep-daily 90 --keep-monthly 3 --repo $restic_repo -p $restic_password
+  restic forget --keep-daily 90 --keep-monthly 3 --repo $restic_repo -p $RESTIC_PWD_FILE
   # Prune the repository
-  restic prune --repo $restic_repo -p $restic_password
+  restic prune --repo $restic_repo -p $RESTIC_PWD_FILE
   fi
-
-  # Clean up old database backup files within the folder $DB_BACKUP_DIR (that's not deleting them directly from restic repo)
-  find "$DB_BACKUP_DIR" -name "*.sql.gz" -type f -mtime +15 -delete
+  # Clean up old database backup files within the folder $DIR_DB_BACKUP (that's not deleting them directly from restic repo)
+  find "$DIR_DB_BACKUP" -name "*.sql.gz" -type f -mtime +15 -delete
+  #Cleanup old log files
+  delete_old_logs
 fi
