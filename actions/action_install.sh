2a2eeb2948f7ba154b54de44a131e189c4bea009
enzo-g <enzo.gautier@gmail.com>
2023-10-28 18:22:16 +0900

    update path variables

--- actions/action_install.sh
+++ actions/action_install.sh
@@ -3,7 +3,7 @@
 # The sequence below is executed if the --install argument is being used.
 echo "Starting the install of restic backup..."
 check_and_backup_existing_files "$RESTIC_CONF" "$RESTIC_PWD_FILE" "$OTHER_DBS_FILE" "$OTHER_PGDBS_FILE" "$EXCLUDED_DIRS_FILE"
-create_directories "$DIR_SCRIPTS" "$DIR_SCRIPT_BACKUP" "$DIR_SCRIPT_LOGS" "$DIR_DB_BACKUP"
+create_directories "$DIR_SCRIPTS" "$DIR_SCRIPT_BACKUP" "$DIR_SCRIPT_LOGS" "$DIR_DB_BACKUP" "$DIR_SCRIPT_BINARIES"
 create_htaccess_file "$DIR_SCRIPT_BACKUP" "$DIR_DB_BACKUP" 
 install_restic
 install_rclone
@@ -12,4 +12,3 @@ create_pgdb_others_file
 create_file_exclude_directory
 create_restic_conf_files
 create_restic_pwd_file
-ensure_script_in_backup_dir
